c71bfe2e36fbe712893de28b5b263cd67f857746
kleberkunha <kleberkunha@hotmail.com>
2021-09-06 12:00:15 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from  'pages/Home/index';
-import Navigation from 'components/Navigation/Navigation';
 import './App.css';
 
 function App() {
   return (
       <>
         <BrowserRouter>
-            <Navigation/>
+
             <Switch>
-              <Route path="/" exact component={Home} />
-              <Route path="/register" exact component={} />
-              <Route path="/login" exact component={} />
-              <Route component={} />
+              <Route path="/Home" exact component={Home}/>
+              <Route path="/register" />
+              <Route path="/login" />
             </Switch>
         </BrowserRouter>
       </>
